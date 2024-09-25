285ed2b109d9bf0dc6b1927f63e56f6abf612270
alesk234 <alejan.maya.h@gmail.com>
2024-09-24 18:12:04 -0600

    primera actividad

---
+++ script.js
@@ -0,0 +1,26 @@
+//Clase 24/09/2024
+//NOMBRE: Maya Hérnandez Alejandro
+//Variables
+
+/*las variables guardan informacion que puedes reutilizar
+*/
+
+//Declaracion de variables
+
+let nombre = 'Pedro'; //usa let para declarar variables que pueden cambiar su valor
+const PI = 3.1416 ;
+var edad = 25;
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b ; //7
+let resta = a - b; //3
+let multiplicaion = a * b;//10
+let division = a/b ; //2.5
+
+//operadores de comparación
+a > b; //true
+a < b; // false
+a == b; // false
+a === b; // false (compara valor y tipo)
